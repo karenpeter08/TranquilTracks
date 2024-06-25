1f7d2a58e4a204c78d144534ca7376406a4f075a
karenpeter08 <karenp5338@gmail.com>
2024-06-25 13:01:33 -0700

    Initial Commit

---
+++ TranquilTracks/TranquilTracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranquilTracksApp.swift
+//  TranquilTracks
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TranquilTracksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
