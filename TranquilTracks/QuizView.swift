6036443ec3742aaa45419e970e0726ce76dc7f00
Rupsaag <rupsgos@gmail.com>
2024-06-26 15:34:01 -0700

    I added a lot.

---
+++ TranquilTracks/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  TranquilTracks
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizView()
+}
---
+++ TranquilTracks/RecsView.swift
@@ -0,0 +1,18 @@
+//
+//  RecsView.swift
+//  TranquilTracks
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+struct RecsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecsView()
+}
