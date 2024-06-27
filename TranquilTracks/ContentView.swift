ac23da11896581793286167b7e676ac1a288598e
Rupsaag <rupsgos@gmail.com>
2024-06-27 09:48:58 -0700

    test 1

--- TranquilTracks/ContentView.swift
+++ TranquilTracks/ContentView.swift
@@ -9,50 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 25) {
-            Spacer()
-            Text("Resources: ")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-           
-            HStack (spacing: 35){
+        NavigationStack {
+            ZStack {
                 
-                
-                
-                Button("Quiz   ") {
+                Image("purple")
+                    .resizable(resizingMode: .stretch)
+                    .frame(height: 999.0)
+                VStack(spacing: 25) {
+                    Spacer()
+                    
+                    Text("W  E  L  C  O  M  E")
+                        .font(.largeTitle)
+                    Text("T           O")
+                        .font(.title2)
+                        .fontWeight(.light)
+                    Image("logo4")
+                        .padding([.bottom, .trailing])
+                    Text("RESOURCES: ")
+                        .font(.largeTitle)
+                        .fontWeight(.light)
+                        .foregroundColor(Color.black)
+                        .padding(.bottom)
+                       
+                    
+                    HStack (spacing: 35){
+                        
+                        
+                        NavigationLink(destination: QuizView()) {
+                            Text("QUIZ      ")
+                                .fontWeight(.thin)
+                        }
+                        .font(.largeTitle)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.purple)
+                        NavigationLink(destination: RecsView()) {
+                            Text("RECS    ")
+                                .fontWeight(.thin)
+                        }
+                        .font(.largeTitle)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.purple)
+                        
+                    } // end hstack
+                    HStack(spacing: 35){
+                        NavigationLink(destination: ResultView()) {
+                            Text("RESULT ")
+                                .fontWeight(.thin)
+                        }
+                        .padding(.leading)
+                        .font(.largeTitle)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.purple)
+                        NavigationLink(destination: InfoView()) {
+                            Text("INFO    ")
+                                .fontWeight(.thin)
+                        }
+                        .font(.largeTitle)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.purple)
+                        
+                        
+                        
+                        .padding(.trailing)
+                    } // end hstack
+                   Text("")
+                    Text("")
+                    Text("")
                     
                 }
-                .font(.largeTitle)
-                .buttonStyle(.borderedProminent)
-                .tint(.purple)
-      
-                
-                Button("Recs   ") {
-                    
-                }
-                .font(.largeTitle)
-                .buttonStyle(.borderedProminent)
-                .tint(.purple)
-                
-            }
-            HStack(spacing: 35){
-                Button("Result") {
-                    
-                }
-                .font(.largeTitle)
-                .buttonStyle(.borderedProminent)
-                .tint(.purple)
-                Button("Info   ") {
-                    
-                }
-                .font(.largeTitle)
-                .buttonStyle(.borderedProminent)
-                .tint(.purple)
-                
-                
-                .padding()
-            }
-            
-        }
+                .padding(.bottom) // end vstack
+            } // end zstack
+        } // end nav stack
     }
 }
 
